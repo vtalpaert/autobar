f52a9b439a01866c4335c964a64b7cfef4d14a16
Victor Talpaert <victor.talpaert@akka.eu>
2018-11-14 19:35:32 +0100

    new cmd

--- hardware/management/commands/manualdemux.py
+++ hardware/management/commands/manualdemux.py
@@ -8,28 +8,17 @@ from hardware.interfaces import GpioInterface
 class Command(BaseCommand):
     help = 'Manually control the demux'
 
-    def add_arguments(self, parser):
-        parser.add_argument('--outputs', help="Use a string to give several outputs as in '0 9 2'")
-        parser.add_argument('--time-on', help='On time [s]', default=1, type=int)
-        parser.add_argument('--time-off', help='Off time [s]', default=1, type=int)
-
     def handle(self, *args, **options):
         interface = GpioInterface.getInstance()
-        raw_outputs = options['outputs']
-        if isinstance(raw_outputs, str):
-            outputs = [int(output) for output in raw_outputs.split()]
-        elif isinstance(raw_outputs, int):
-            outputs = [raw_outputs]
-        else:
-            outputs = []
         try:
-            for output in outputs:
+            while True:
+                output = int(input('Output number: '))
+                t = float(input('Time [s]: '))
                 interface.demux_start(output)
                 print('demux: %i is on !' % output)
-                sleep(options['time_on'])
+                sleep(t)
                 interface.demux_stop(output=output)
                 print('demux: %i is off' % output)
-                sleep(options['time_off'])
         finally:
             print('demux: all stop')
             interface.demux_stop()
