5acfea6e0eaf1d72fe4f02a1e1e6658c10e5a3f2
Victor Talpaert <victor.talpaert@akka.eu>
2018-11-20 18:57:39 +0100

    add sort_by page mechanism

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from recipes import views
 
 urlpatterns = [
+    path('mixes/<slug:sort_by>/', views.Mixes.as_view(), name='mixes_s'),
     path('mixes/', views.Mixes.as_view(), name='mixes'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
 from django.views.generic.base import TemplateView
+
+
+def get_or_none(dic, key):
+    try:
+        return dic[key]
+    except KeyError:
+        return None
 
 
 class Mixes(TemplateView):
@@ -8,4 +14,13 @@ class Mixes(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
+        sort_by = get_or_none(kwargs, 'sort_by')
+        sorts = ['A-Z', 'Popularity']
+        if sort_by not in sorts:
+            sort_by = sorts[0]
+
+        context['sorts'] = sorts
+        context['sort_by'] = sort_by
+
         return context
