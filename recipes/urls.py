7edde8950128a9b1955949714acb1b25ab7c09a0
Victor Talpaert <victor.talpaert@akka.eu>
2018-11-22 17:17:45 +0100

    added subsorting

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from recipes import views
 
 urlpatterns = [
+    path('mixes/<slug:sort_by>/<slug:subsort_by>/', views.Mixes.as_view(), name='mixes_ss'),
     path('mixes/<slug:sort_by>/', views.Mixes.as_view(), name='mixes_s'),
     path('mixes/', views.Mixes.as_view(), name='mixes'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,23 @@
+from collections import OrderedDict
+
 from django.views.generic.base import TemplateView
+
+from recipes.models import Mix
+
+
+order_by = OrderedDict((
+    ('A-Z', OrderedDict((
+        ('A-Z', 'name'),
+        ('Z-A', '-name'),
+    ))),
+    ('Popularity', OrderedDict((
+        ('Likes', '-likes'),
+        ('Count', '-count'),
+    ))),
+    ('Recent', OrderedDict((
+        ('Updated', '-updated_at'),
+    ))),
+))
 
 
 def get_or_none(dic, key):
@@ -14,13 +33,21 @@ class Mixes(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        mixes = Mix.objects.filter(verified=True)  # TODO filter for availables
 
+        print(order_by)
         sort_by = get_or_none(kwargs, 'sort_by')
-        sorts = ['A-Z', 'Popularity']
+        sorts = list(order_by.keys())
         if sort_by not in sorts:
             sort_by = sorts[0]
+        subsort_by = get_or_none(kwargs, 'subsort_by')
+        subsorts = list(order_by[sort_by].keys())
+        if subsort_by not in subsorts:
+            subsort_by = subsorts[0]
 
         context['sorts'] = sorts
         context['sort_by'] = sort_by
+        context['subsorts'] = subsorts
+        context['subsort_by'] = subsort_by
 
         return context
