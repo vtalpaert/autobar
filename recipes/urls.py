5e57c1221aa74d0c4fa67f62f86a0bddeb1db22d
Victor Talpaert <victor.talpaert@akka.eu>
2018-11-29 14:43:24 +0100

    index redirects to mixes url

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 from recipes import views
 
@@ -6,4 +7,5 @@ urlpatterns = [
     path('mixes/<slug:sort_by>/<slug:subsort_by>/', views.Mixes.as_view(), name='mixes_ss'),
     path('mixes/<slug:sort_by>/', views.Mixes.as_view(), name='mixes_s'),
     path('mixes/', views.Mixes.as_view(), name='mixes'),
+    path('', RedirectView.as_view(url='mixes', permanent=False), name='index'),
 ]
